7a56c7d2c3e0119bbd4d112507202c15ae6a93ac
Lu <lucial85@gmail.com>
2024-03-05 16:59:02 -0300

    add script dml

---
+++ database/dml.sql
@@ -0,0 +1,24 @@
+INSERT INTO team (name, state, photo_url) VALUES ('Flamengo', 'RJ', 'https://upload.wikimedia.org/wikipedia/commons/9/93/Flamengo-RJ_%28BRA%29.png');
+INSERT INTO team (name, state, photo_url) VALUES ('São Paulo', 'SP', 'https://upload.wikimedia.org/wikipedia/commons/4/4b/S%C3%A3o_Paulo_Futebol_Clube.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Palmeiras', 'SP', 'https://w7.pngwing.com/pngs/410/494/png-transparent-sociedade-esportiva-palmeiras-campeonato-brasileiro-serie-a-campeonato-paulista-allianz-parque-football-football-team-logo-sports.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Santos', 'SP', 'https://w1.pngwing.com/pngs/715/309/png-transparent-graphy-logo-santos-fc-football-campeonato-brasileiro-serie-a-sports-copa-do-brasil-sports-association-brazil-white-black.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Grêmio', 'RS', 'https://i.pinimg.com/originals/08/59/2c/08592c039ce09051e8b750bab1539fdd.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Internacional', 'RS', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Escudo_do_Sport_Club_Internacional.svg/600px-Escudo_do_Sport_Club_Internacional.svg.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Vasco', 'RJ', 'https://w7.pngwing.com/pngs/714/278/png-transparent-vasco-da-gama-hd-logo-thumbnail.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Botafogo', 'RJ', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Escudo_Botafogo.png/800px-Escudo_Botafogo.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Atlético Mineiro', 'MG', 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Atletico_mineiro_galo.png');
+INSERT INTO team (name, state, photo_url) VALUES ('Cruzeiro', 'MG', 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Logo_Cruzeiro_1996.png');
+
+
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-01', 1, 2, 2, 1, 1);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-02', 3, 3, 4, 0, 3);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-03', 5, 1, 6, 1, 5);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-04', 7, 0, 8, 2, 8);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-05', 9, 2, 10, 1, 10);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-06', 1, 1, 3, 1, 3);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-07', 2, 2, 4, 2, 2);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-08', 5, 3, 6, 0, 5);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-09', 7, 1, 9, 1, 7);
+INSERT INTO match (date, team_one_id, score_team_one, team_two_id, score_team_two, team_supoorted_id) VALUES ('2024-03-10', 8, 2, 10, 2, 8);
+
+
